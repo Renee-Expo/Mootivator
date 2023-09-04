71ef84df66c4d9af2c5c985d048a28cb5f637ec0
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-09-04 15:27:32 +0800

    Created MainView.swift

--- Ratatouille/Views/ContentView.swift
+++ Ratatouille/Views/ContentView.swift
@@ -7,15 +7,11 @@
 
 import SwiftUI
 
+// Don't code in this View, its the main holding view, so tab bars, sidebars.
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        MainView()
     }
 }
 
