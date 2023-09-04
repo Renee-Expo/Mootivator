1226acc9f0582ca7c7c84aad62cefb906551792c
Renee <renee.teh10@rafflesgirlssch.edu.sg>
2023-09-04 12:44:14 +0800

    Initial Commit

---
+++ Ratatouille/RatatouilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatatouilleApp.swift
+//  Ratatouille
+//
+//  Created by rgs on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct RatatouilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
