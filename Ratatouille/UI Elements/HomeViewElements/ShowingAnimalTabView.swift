e3496482a16b1718ae88755dc42b952770d23f9b
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-11-18 12:19:24 +0800

    added AnimalEmotionElement.swift , an element for the home screen.

---
+++ Ratatouille/UI Elements/HomeViewElements/ShowingAnimalTabView.swift
@@ -0,0 +1,18 @@
+//
+//  ShowingAnimalTabView.swift
+//  Ratatouille
+//
+//  Created by klifton Cheng stu on 18/11/23.
+//
+
+import SwiftUI
+
+struct ShowingAnimalTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowingAnimalTabView()
+}
