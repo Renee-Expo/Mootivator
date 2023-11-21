3b796635ec348935ea173dacb6041eb3a1dfd3f5
Kaveri Mi <kaverizm@gmail.com>
2023-11-21 11:06:35 +0800

    Added HabitCompletionView and GoalCompletionView

---
+++ Ratatouille/Views/HabitCompletionView.swift
@@ -0,0 +1,20 @@
+//
+//  HabitCompletionView.swift
+//  Ratatouille
+//
+//  Created by Kaveri Mi on 21/11/23.
+//
+
+import SwiftUI
+
+struct HabitCompletionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HabitCompletionView_Previews: PreviewProvider {
+    static var previews: some View {
+        HabitCompletionView()
+    }
+}
