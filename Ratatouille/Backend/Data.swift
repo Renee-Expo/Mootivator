3943f3a40c010db6b4334010a409e2cd45d2e69e
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-11-22 10:06:52 +0800

    Move items from goal.swift to Data.swift, deleted goal.swift

--- Ratatouille/Backend/Data.swift
+++ Ratatouille/Backend/Data.swift
@@ -35,11 +35,50 @@ struct Animal : Identifiable, Codable {
     var kind : AnimalSelection         // what type/kind/species the animal is
 }
 // change to Goal from Goal.swift? 
+/*
 struct GoalItem : Identifiable, Codable{
     var id = UUID()
     
     var title : String
     var animal : Animal
+}
+*/
+
+struct Goal: Identifiable, Codable {
+    var id = UUID ()
+    
+    var goalEntered : String
+    var deadline : Date
+    var habitEntered : String
+    var frequencyOfHabits : String
+    var selectedAnimal : Int
+    var frequency : Array<String>
+    var motivationalQuote : String
+    var selectedFrequencyIndex : Int
+    var selectedDailyDeadline : Date
+    var numberOfTimesPerWeek : Int
+    var numberOfTimesPerMonth : Int
+    var days : Array<String>
+    var selectedFixedDeadline : Date
+    var isSaveButtonDisabled : Bool     // Save button is still work in progress
+    var mondayChosen : Bool     // really inefficient section.
+    var tuesdayChosen : Bool
+    var wednesdayChosen : Bool
+    var thursdayChosen : Bool
+    var fridayChosen : Bool
+    var saturdayChosen : Bool
+    var sundayChosen : Bool
+    
+}
+
+extension Goal {
+    
+    //Todo.sampleTodos
+    static let sampleGoals = [
+        Goal(goalEntered: "Get A for Math", habitEntered: "Do one Math practice paper daily"),
+        Goal(goalEntered: "Lead a healthier life", habitEntered: "Run 2km")
+    ]
+    
 }
 
 var autonomy: Double = 0 // not sure if this will/can be used
