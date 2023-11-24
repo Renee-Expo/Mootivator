43f0c2041ea4059a4bb9419cf216b291030eb0cb
zi qi <jiangzziqi@gmail.com>
2023-11-24 17:42:30 +0800

    added AnimateProgressView

---
+++ Ratatouille/Views/AnimateProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimateProgressView.swift
+//  Ratatouille
+//
+//  Created by T Krobot on 24/11/23.
+//
+
+import SwiftUI
+
+struct AnimateProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimateProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimateProgressView()
+    }
+}
