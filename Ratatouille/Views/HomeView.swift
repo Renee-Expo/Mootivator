db61dd7cf7c54ca1a3b86793c53da6e151211d1c
zi qi <jiangzziqi@gmail.com>
2023-11-25 01:24:38 +0800

    HabitCompletionView shows up when date is clicked

--- Ratatouille/Views/HomeView.swift
+++ Ratatouille/Views/HomeView.swift
@@ -16,76 +16,74 @@ struct HomeView: View {
     @State var indexItem : Int = 0
     @State var selectedDate : Date = Date()
     @State private var showMarkHabitCompletionAlert = false
+    @State private var showHabitCompletionView = false
     @Binding var habitTitle : String
     @Binding var title : String
     
     var body: some View {
-        VStack {
-//            AnimalEmotionElement(scale: .constant(20))
-//            Text("Metric == \(indexItem)") // for debugging
-            
-            HStack {
-                Button {
-                    // Move left
-                } label: {
-                    Image(systemName: "chevron.compact.left")
-                        .resizable()
+        NavigationView {
+            VStack {
+                HStack {
+                    Button {
+                        // Move left
+                    } label: {
+                        Image(systemName: "chevron.compact.left")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: chevronWidth)
+                    }
+                    Spacer()
+                    
+                    // Replace ShowingAnimalSegmentedControlElement with your segmented control
+                    ShowingAnimalSegmentedControlElement(selection: $indexItem)
+                        .frame(width: 200)
                         .scaledToFit()
-                        .frame(width: chevronWidth)
+                    
+                    Button {
+                        // move right
+                    } label: {
+                        Image(systemName: "chevron.compact.right")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: chevronWidth)
+                    }
                 }
+                .padding(.horizontal)
+                
                 Spacer()
-                // add image view here? should be segmented control
-//                Image(systemName: "photo")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .padding()
                 
-                ShowingAnimalSegmentedControlElement(selection: $indexItem)
-                    .frame(width: 200)
-                    .scaledToFit()
-                
-                Button {
-                    // move right
-                } label: {
-                    Image(systemName: "chevron.compact.right")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: chevronWidth)
+                HStack {
+                    Text("\(title)")
+                        .font(.system(size: 24))
+                        .fontWeight(.medium)
                 }
-            }
-            .padding(.horizontal)
-            
-            Spacer()
-            // Sheetview here, find a way to remove the darkening background and covering of the navigation bar
-            
-            HStack {
-                Text("\(title)")
-                    .font(.system(size: 24))
-                    .fontWeight(.medium)
-            }
-            
-            DatePicker(selection: $selectedDate, displayedComponents: .date) {
-                Text("Select a date")
-            }
-            .datePickerStyle(.graphical)
-            .padding(10)
-            .onChange(of: selectedDate) { _ in
-                showMarkHabitCompletionAlert = true
-            }
-            .alert(isPresented: $showMarkHabitCompletionAlert) {
-                Alert(
-                    title: Text("Mark \(habitTitle) as Completed?"),
-                    primaryButton: .default(Text("Yes")) {
-                        //goalmanager.markHabitCompleted(for: selectedDate)
-                    },
-                    secondaryButton: .cancel(Text("No"))
-                )
+                
+                DatePicker(selection: $selectedDate, displayedComponents: .date) {
+                    Text("Select a date")
+                }
+                .datePickerStyle(.graphical)
+                .padding(10)
+                .onChange(of: selectedDate) { _ in
+                    showMarkHabitCompletionAlert = true
+                }
+                .alert(isPresented: $showMarkHabitCompletionAlert) {
+                    Alert(
+                        title: Text("Mark \(habitTitle) as Completed?"),
+                        primaryButton: .default(Text("Yes")) {
+                            showHabitCompletionView = true
+                            // goalmanager.markHabitCompleted(for: selectedDate)
+                        },
+                        secondaryButton: .cancel(Text("No"))
+                    )
+                }
+                NavigationLink(
+                    destination:         HabitCompletionView(frequency: .constant(["Fixed", "Daily", "Weekly", "Monthly"]), selectedFrequencyIndex: .constant(0), selectedDailyDeadline:.constant(Date()), selectedFixedDeadline: .constant(Date()), isHabitCompleted: true),
+                    isActive: $showHabitCompletionView
+                ) {
+                    EmptyView()
+                }
             }
         }
-//        .background {
-//            Color(.backgroundColors)
-//                .ignoresSafeArea()
-//        }
     }
 }
 
@@ -95,3 +93,4 @@ struct HomeView_Previews: PreviewProvider {
             .environmentObject(GoalManager())
     }
 }
+
