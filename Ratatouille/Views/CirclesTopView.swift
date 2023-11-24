15ee3b1709bcb00837cf6e9658b9a81a9e6e040b
zi qi <jiangzziqi@gmail.com>
2023-11-24 17:41:41 +0800

    added CirclesTopView

---
+++ Ratatouille/Views/CirclesTopView.swift
@@ -0,0 +1,20 @@
+//
+//  CirclesTopView.swift
+//  Ratatouille
+//
+//  Created by T Krobot on 24/11/23.
+//
+
+import SwiftUI
+
+struct CirclesTopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CirclesTopView_Previews: PreviewProvider {
+    static var previews: some View {
+        CirclesTopView()
+    }
+}
