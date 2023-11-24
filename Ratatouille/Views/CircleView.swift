b9eb8cd9618b047bd7f74e759ec5d09d770a04ad
zi qi <jiangzziqi@gmail.com>
2023-11-24 17:38:01 +0800

    added CircleView

---
+++ Ratatouille/Views/CircleView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleView.swift
+//  Ratatouille
+//
+//  Created by T Krobot on 24/11/23.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleView()
+    }
+}
