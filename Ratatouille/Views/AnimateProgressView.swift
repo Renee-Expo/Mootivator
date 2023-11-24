17089684c75215f9b7fe31ea1d3c15fbffbb415e
zi qi <jiangzziqi@gmail.com>
2023-11-24 17:48:40 +0800

    progress of bar and value change according to value

--- Ratatouille/Views/AnimateProgressView.swift
+++ Ratatouille/Views/AnimateProgressView.swift
@@ -4,14 +4,35 @@
 //
 //  Created by T Krobot on 24/11/23.
 //
-
 import SwiftUI
 
 struct AnimateProgressView: View {
+    @State private var progress: Double = 42.0
+
+    var minProgress = 0.0
+    var maxProgress = 100.0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack() {
+            CircularProgressView(progress: self.progress / 100)
+                .animation(.linear)
+
+            VStack {
+            Text("Progress: \(progress, specifier: "%.1f")")
+                Slider(value: $progress,
+                       in: minProgress...maxProgress,
+                       minimumValueLabel: Text("0"),
+                       maximumValueLabel: Text("100")
+                ) {}
+            }
+            .padding()
+
+            Spacer()
+        }
+        .padding(20)
     }
 }
+
 
 struct AnimateProgressView_Previews: PreviewProvider {
     static var previews: some View {
--- Ratatouille/Views/CirclesTopView.swift
+++ Ratatouille/Views/CirclesTopView.swift
@@ -5,11 +5,54 @@
 //  Created by T Krobot on 24/11/23.
 //
 
+
 import SwiftUI
 
 struct CirclesTopView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack() {
+            CircularProgressView(progress: 0.55)
+
+            CircularProgressView(progress: 0.85)
+                .scaleEffect(0.75)
+
+            Spacer()
+        }
+        .padding(20)
+    }
+}
+
+struct CircularProgressView: View {
+    var progress: Double
+
+    var body: some View {
+        let progressText = String(format: "%.0f%%", progress * 100)
+        let purpleAngularGradient = AngularGradient(
+            gradient: Gradient(colors: [
+                Color(red: 200/255, green: 168/255, blue: 240/255),
+                Color(red: 71/255, green: 33/255, blue: 158/255)
+            ]),
+            center: .center,
+            startAngle: .degrees(0),
+            endAngle: .degrees(360.0 * progress))
+
+        ZStack {
+            Circle()
+                .stroke(Color(.systemGray4), lineWidth: 20)
+            Circle()
+                .trim(from: 0, to: CGFloat(self.progress))
+                .stroke(
+                    purpleAngularGradient,
+                    style: StrokeStyle(lineWidth: 20, lineCap: .round))
+                .rotationEffect(Angle(degrees: -90))
+                .overlay(
+                    Text(progressText)
+                        .font(.system(size: 56, weight: .bold, design:.rounded))
+                        .foregroundColor(Color(.systemGray))
+                )
+        }
+        .frame(width: 200, height: 200)
+        .padding()
     }
 }
 
