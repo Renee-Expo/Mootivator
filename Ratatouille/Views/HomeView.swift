59a89d376128b6981e95c704d1cb09edbcc348c2
Renee <renee.teh10@rafflesgirlssch.edu.sg>
2023-11-25 10:28:56 +0800

    Worked on marking daily habits as complete using calendar in homeView.

--- Ratatouille/Views/HomeView.swift
+++ Ratatouille/Views/HomeView.swift
@@ -19,7 +19,8 @@ struct HomeView: View {
     @State private var showHabitCompletionView = false
     @Binding var habitTitle : String
     @Binding var title : String
-    
+    @State var dailyHabitCompletionStatus: [Date: Bool] = [:]
+
     var body: some View {
         NavigationView {
             VStack {
@@ -71,17 +72,17 @@ struct HomeView: View {
                         title: Text("Mark \(habitTitle) as Completed?"),
                         primaryButton: .default(Text("Yes")) {
                             showHabitCompletionView = true
-                            // goalmanager.markHabitCompleted(for: selectedDate)
+                            dailyHabitCompletionStatus[selectedDate] = true
                         },
                         secondaryButton: .cancel(Text("No"))
                     )
                 }
-                NavigationLink(
-                    destination:         HabitCompletionView(frequency: .constant(["Fixed", "Daily", "Weekly", "Monthly"]), selectedFrequencyIndex: .constant(0), selectedDailyDeadline:.constant(Date()), selectedFixedDeadline: .constant(Date()), isHabitCompleted: true),
-                    isActive: $showHabitCompletionView
-                ) {
-                    EmptyView()
-                }
+//                NavigationLink(
+//                    destination:         HabitCompletionView(frequency: .constant(["Fixed", "Daily", "Weekly", "Monthly"]), selectedFrequencyIndex: .constant(0), selectedDailyDeadline:.constant(Date()), selectedFixedDeadline: .constant(Date()), isHabitCompleted: true),
+//                    isActive: $showHabitCompletionView
+//                ) {
+//                    EmptyView()
+//                }
             }
         }
     }
@@ -93,3 +94,151 @@ struct HomeView_Previews: PreviewProvider {
             .environmentObject(GoalManager())
     }
 }
+
+
+//import SwiftUI
+//
+//struct HomeView: View {
+//
+//    @EnvironmentObject var goalManager: GoalManager
+//    @Environment(\.colorScheme) var colorScheme
+//
+//    var chevronWidth : Double = 15
+//    @State var indexItem : Int = 0
+//    @State var selectedDate = Date()
+//    @State private var showMarkHabitCompletionAlert = false
+//    @Binding var habitTitle : String
+//    @Binding var title : String
+//    @Binding var isHabitCompleted : Bool
+//
+//    var body: some View {
+//        VStack {
+//            //            AnimalEmotionElement(scale: .constant(20))
+//            //            Text("Metric == \(indexItem)") // for debugging
+//
+//            HStack {
+//                Button {
+//                    // Move left
+//                } label: {
+//                    Image(systemName: "chevron.compact.left")
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: chevronWidth)
+//                }
+//                Spacer()
+//                // add image view here? should be segmented control
+//                //                Image(systemName: "photo")
+//                //                    .resizable()
+//                //                    .scaledToFit()
+//                //                    .padding()
+//
+//                ShowingAnimalSegmentedControlElement(selection: $indexItem)
+//                    .frame(width: 200)
+//                    .scaledToFit()
+//
+//                Button {
+//                    // move right
+//                } label: {
+//                    Image(systemName: "chevron.compact.right")
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: chevronWidth)
+//                }
+//            }
+//            .padding(.horizontal)
+//
+//            Spacer()
+//            // Sheetview here, find a way to remove the darkening background and covering of the navigation bar
+//
+//            HStack {
+//                Text("\(title)")
+//                    .font(.system(size: 24))
+//                    .fontWeight(.medium)
+//            }
+//
+//            DatePicker(selection: $selectedDate, displayedComponents: .date) {
+//                Text("Select a date")
+//            }
+//            .datePickerStyle(.graphical)
+//            .padding(10)
+//            .onTapGesture {
+//                if selectedDate <= Date() {
+//                    showMarkHabitCompletionAlert = true
+//                }
+//            }
+//            .alert(isPresented: $showMarkHabitCompletionAlert) {
+//                Alert(
+//                    title: Text("Mark \(habitTitle) as Completed?"),
+//                    primaryButton: .default(Text("Yes")) {
+//                        isHabitCompleted = true
+//                    },
+//                    secondaryButton: .cancel(Text("No"))
+//                )
+//            }
+//
+//            .overlay(
+//                Circle()
+//                    .foregroundColor(isHabitCompleted ? Color.green.opacity(0.3) : Color.red.opacity(0.3))
+//                    .frame(width: 30, height: 30)
+//            )
+//            .onChange(of: selectedDate) { _ in
+//                showMarkHabitCompletionAlert = false
+//            }
+//
+//        }
+//        //        .background {
+//        //            Color(.backgroundColors)
+//        //                .ignoresSafeArea()
+//        //        }
+//    }
+//}
+//
+//struct HomeView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HomeView(habitTitle: .constant("Sample Habit Title"), title: .constant("Sample Title"),isHabitCompleted: .constant(true))
+//            .environmentObject(GoalManager())
+//    }
+//}
+//
+////            .datePickerStyle(.graphical)
+////            .padding()
+////
+////            //CalendarView(selectedDate: $selectedDate, isHabitCompleted: $isHabitCompleted)
+//////            .overlay(
+//////                Circle()
+//////                    .foregroundColor(isHabitCompleted ? Color.green.opacity(0.3) : Color.red.opacity(0.3))
+//////                    .frame(width: 30, height: 30)
+//////                    .position(x:UIScreen.main.bounds.width / 2, y: 250)
+//////                    .onTapGesture {
+//////                        if selectedDate <= Date() {
+//////                        showMarkHabitCompletionAlert = true
+//////                    }
+//////                }
+//////            )
+////            .onChange(of: selectedDate) { _ in
+////                showMarkHabitCompletionAlert = false
+////            }
+////            .alert(isPresented: $showMarkHabitCompletionAlert) {
+////                Alert(
+////                    title: Text("Mark \(habitTitle) as Completed?"),
+////                    primaryButton: .default(Text("Yes")) {
+////                        //goalmanager.markHabitCompleted(for: selectedDate)
+////                    },
+////                    secondaryButton: .cancel(Text("No"))
+////                )
+////            }
+////        }
+//////        .background {
+//////            Color(.backgroundColors)
+//////                .ignoresSafeArea()
+//////        }
+////    }
+////}
+////
+////struct HomeView_Previews: PreviewProvider {
+////    static var previews: some View {
+////        HomeView(habitTitle: .constant("Sample Habit Title"), title: .constant("Sample Title"),isHabitCompleted: .constant(true))
+////            .environmentObject(GoalManager())
+////    }
+////}
+//
