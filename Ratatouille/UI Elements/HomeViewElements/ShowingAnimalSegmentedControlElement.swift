aebb038666a58e7e4c77e0b1aba063acd9cf6c0e
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-11-27 16:25:18 +0800

    updated ShowingAnimgalSegmentedControlElement.swift

--- Ratatouille/UI Elements/HomeViewElements/ShowingAnimalSegmentedControlElement.swift
+++ Ratatouille/UI Elements/HomeViewElements/ShowingAnimalSegmentedControlElement.swift
@@ -11,18 +11,24 @@ struct ShowingAnimalSegmentedControlElement: View {
     
     @EnvironmentObject var goalItemList : GoalManager
     @Binding var selection : Int // controlled by a swipeGesture/Button to increment/decrement for selection of the correct animal
+//    @Binding var goalItem : Goal
     
     var body: some View {
         VStack {
-            Picker(selection: $selection) {
-                ForEach($goalItemList.goals, id: \.id ) { $goal in
-//                    VStack {
-//                        Text(goal.selectedAnimal.name)
-//                    }
+            //            Picker(selection: $selection) {
+            ForEach($goalItemList.goals, id: \.id ) { $goal in
+                VStack {
+                    //                        Text(goal.selectedAnimal.name) // this should be navigation title on homeview
+                    Image("\(goalItemList.goals[selection].selectedAnimal.kind.image)" + "\(Animal.emotion.self)")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                        .padding()
                 }
-            } label: {
-                Text("Segmented Control")
             }
+//            } label: {
+//                Text("Segmented Control")
+//            }
             .pickerStyle(.segmented)
 
         }
