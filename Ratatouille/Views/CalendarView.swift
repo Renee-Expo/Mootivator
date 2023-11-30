bb13aa8ea2e57493f254ca9bfdb2a615c792b10c
zi qi <jiangzziqi@gmail.com>
2023-12-01 02:09:38 +0800

    added CalendarView

---
+++ Ratatouille/Views/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  Ratatouille
+//
+//  Created by T Krobot on 1/12/23.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
