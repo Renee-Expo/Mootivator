7b6923b1ec0feee01c82b0cf1e8af16c7dec958e
zi qi <jiangzziqi@gmail.com>
2023-12-03 18:58:29 +0800

    changed some ui in home view

--- Ratatouille/UI Elements/HomeViewElements/ShowingAnimalSegmentedControlElement.swift
+++ Ratatouille/UI Elements/HomeViewElements/ShowingAnimalSegmentedControlElement.swift
@@ -16,73 +16,82 @@ struct ShowingAnimalSegmentedControlElement: View {
     
     var body: some View {
 //        var colorInverted = (colorScheme == .dark ? Color.black : Color.white)
-        VStack {
-            if goalManager.items.count > 0 {
-                TabView {
-                    ForEach($goalManager.items, id: \.id ) { $item in
-                        ScrollView {
-                            AnimalEmotionElement(goal: item)
-                            VStack(alignment: .leading) {
-                                Text("\(item.title)")
-                                    .font(.system(.title2))
-                                    .fontWeight(.heavy)
-                                Text("\(item.habitTitle)")
-                                    .fontWeight(.medium)
+        ZStack {
+            Color("BackgroundColors")
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                if goalManager.items.count > 0 {
+                    TabView {
+                        ForEach($goalManager.items, id: \.id ) { $item in
+                            ScrollView {
+                                AnimalEmotionElement(goal: item)
+                                VStack(alignment: .center) {
+                                    Text("\(item.title)")
+                                        .font(.system(.title2))
+                                        .fontWeight(.heavy)
+                                    Text("\(item.habitTitle)")
+                                        .fontWeight(.medium)
+                                }
+    //                                .clipShape(Circle())
+                                VStack {
+                                    Image("\(item.selectedAnimal.kind.image)" + "\(item.selectedAnimal.emotion.text)")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .padding(.horizontal)
+        //                                .frame(width: 200)
+                                        .background(Color("BackgroundColors"))
+                                    Text(item.selectedAnimal.name)
+                                    Text("\(item.motivationalQuote)")
+                                        .font(.footnote)
+                                        .fontWeight(.medium)
+                                        .padding(10)
+                                }
+    //                                .padding(.leading, 100)
+    //                                .padding( )
+    //                                .multilineTextAlignment(.trailing)
+                                
+    //                            Spacer()
+                                RoundedRectangle(cornerRadius: 30)
+                                    .frame(width: 400, height: 350)
+                                    .foregroundColor(.white)
+                                    .overlay(
+                                CalendarView(selectedDate: Date(), goal: $item)
+                                    .scaledToFit()
+                                    .frame(width: 300, height: 300)
+                                    .padding()
+                                )
+                                
+    //                            HStack {
+    //                                Spacer()
+    //                                Text("\(item.motivationalQuote)")
+    //                                    .font(.footnote)
+    //                                    .fontWeight(.medium)
+    //                                    .padding(.bottom, 200)
+    //                                    .padding(.leading, 100)
+    //                                    .padding( )
+    //                                    .multilineTextAlignment(.trailing)
+    //                            }
                             }
-                            
-                            Image("\(item.selectedAnimal.kind.image)" + "\(item.selectedAnimal.emotion.text)")
-                                .resizable()
-                                .scaledToFit()
-                                .padding(.horizontal)
-//                                .frame(width: 200)
-                                .background(Color("BackgroundColors"))
-//                                .clipShape(Circle())
-                                .padding(.bottom, 10)
-                            Text(item.selectedAnimal.name)
-                            Text("\(item.motivationalQuote)")
-                                .font(.footnote)
-                                .fontWeight(.medium)
-                                .padding(.top, 10)
-//                                .padding(.leading, 100)
-//                                .padding( )
-//                                .multilineTextAlignment(.trailing)
-                            
-//                            Spacer()
-                            
-                            CalendarView(selectedDate: Date(), goal: $item)
-                                .scaledToFit()
-                                .padding()
-                            
-//                            HStack {
-//                                Spacer()
-//                                Text("\(item.motivationalQuote)")
-//                                    .font(.footnote)
-//                                    .fontWeight(.medium)
-//                                    .padding(.bottom, 200)
-//                                    .padding(.leading, 100)
-//                                    .padding( )
-//                                    .multilineTextAlignment(.trailing)
-//                            }
+                            .scrollIndicators(.never)
+                            .frame(width: 400)
                         }
-                        .scrollIndicators(.never)
-                        .frame(width: 400)
                     }
-                }
-                .tabViewStyle(PageTabViewStyle())
-                .indexViewStyle(.page(backgroundDisplayMode: .never))
-                .padding()
-                .scaledToFill()
-                .overlay {
-                    VStack {
-                        Spacer()
-//                        Rectangle()
-//                           .fill(.linearGradient(Gradient(colors: [colorInverted, colorInverted, .clear]), startPoint: .bottom, endPoint: .top))
-//                            .frame(height: 50)
-//                            .scaledToFill()
+                    .tabViewStyle(PageTabViewStyle())
+                    .indexViewStyle(.page(backgroundDisplayMode: .never))
+                    .padding()
+                    .scaledToFill()
+                    .overlay {
+                        VStack {
+                            Spacer()
+    //                        Rectangle()
+    //                           .fill(.linearGradient(Gradient(colors: [colorInverted, colorInverted, .clear]), startPoint: .bottom, endPoint: .top))
+    //                            .frame(height: 50)
+    //                            .scaledToFill()
+                        }
                     }
+                } else {
+                    Text("You have no current goals!")
                 }
-            } else {
-                Text("You have no current goals!")
             }
         }
     }
